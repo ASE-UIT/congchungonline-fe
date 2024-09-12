fdd649ad5bcccc900ee0ec1dbe796be492e241a2
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-12 15:43:45 +0700

    Refactor routing and lazy loading components
    - Refactored the routing in App.js to use the react-router-dom's Routes and Route components.
    - Implemented lazy loading for SignIn and SignUp components in App.js.
    - Updated import paths for SignIn and SignUp components in App.js.
    - Updated import paths and component names in ForgotPassword.js and SignIn.js.
    - Added email input change handler in ForgotPassword.js.
    - Added sendResetPassword function in ForgotPassword.js.
    - Updated toggleColorMode and toggleCustomTheme functions in SignIn.js.
    - Updated theme mode initialization in SignIn.js.
    - Updated background styles in SignIn.js.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-import SignIn from "./pages/signin/SignIn";
-import SignUp from "./pages/signup/SignUp";
 import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 
+const SignIn = lazy(() => import("./pages/Signin/SignIn"));
+const SignUp = lazy(() => import("./pages/Signup/SignUp"));
 function App() {
-  return (
-    <Routes>
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+        </Routes>
+    );
 }
 
 export default App;
