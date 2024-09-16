93ecdee90f2ef64eafa5226afe5fa4c1060dbea5
not-thanhtai <tai606760@gmail.com>
2024-09-16 12:40:34 +0700

    config theme for app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 
-const SignIn = lazy(() => import("./pages/Signin/SignIn"));
-const SignUp = lazy(() => import("./pages/Signup/SignUp"));
+const SignIn = lazy(() => import("./pages/signin/SignIn"));
+const SignUp = lazy(() => import("./pages/signup/SignUp"));
 function App() {
-    return (
-        <Routes>
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="/signin" element={<SignIn />} />
+			<Route path="/signup" element={<SignUp />} />
+		</Routes>
+	);
 }
 
 export default App;
