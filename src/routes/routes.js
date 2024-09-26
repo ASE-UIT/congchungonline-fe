9224b7978766ca428b9ad8e97b2e596cf2816e05
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-26 12:34:05 +0700

    Add NotFound component for handling 404 errors

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,36 +1,42 @@
 import { lazy } from "react";
 
 const routes = [
-	{
-		path: "/",
-		element: lazy(() => import("../pages/home/Home")),
-		authRequired: false,
-	},
-	{
-		path: "/services",
-		element: lazy(() => import("../pages/services/Services")),
-		authRequired: false,
-	},
-	{
-		path: "/signin",
-		element: lazy(() => import("../pages/signin/SignIn")),
-		authRequired: false,
-	},
-	{
-		path: "/signup",
-		element: lazy(() => import("../pages/signup/SignUp")),
-		authRequired: false,
-	},
-	{
-		path: "/profile",
-		element: lazy(() => import("../pages/profile/UserProfile")),
-		authRequired: true,
-	},
-	{
-		path: "/notarization",
-		element: lazy(() => import("../pages/services/CreateNotarizationProfile")),
-		authRequired: true,
-	},
+    {
+        path: "/",
+        element: lazy(() => import("../pages/home/Home")),
+        authRequired: false,
+    },
+    {
+        path: "/services",
+        element: lazy(() => import("../pages/services/Services")),
+        authRequired: false,
+    },
+    {
+        path: "/signin",
+        element: lazy(() => import("../pages/signin/SignIn")),
+        authRequired: false,
+    },
+    {
+        path: "/signup",
+        element: lazy(() => import("../pages/signup/SignUp")),
+        authRequired: false,
+    },
+    {
+        path: "/profile",
+        element: lazy(() => import("../pages/profile/UserProfile")),
+        authRequired: true,
+    },
+    {
+        path: "/notarization",
+        element: lazy(() =>
+            import("../pages/services/CreateNotarizationProfile")
+        ),
+        authRequired: true,
+    },
+    {
+        path: "*",
+        element: lazy(() => import("../pages/notfound/NotFound")),
+    },
 ];
 
 export default routes;
