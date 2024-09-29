fabf01b53171e47ef7425074f915be8e8e806eac
not-thanhtai <tai606760@gmail.com>
2024-09-29 23:58:29 +0700

    Refactor authentication, add axios configuration

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,24 +1,76 @@
-import axios from "axios";
-import { AUTH_ENDPOINT } from "./config";
+import axios from 'axios';
+import { API_BASE_URL } from './config';
+import Cookies from 'js-cookie';
+import axiosConfig from './axiosConfig';
+
+const AUTH_ENDPOINT = `${API_BASE_URL}/auth`;
 
 const login = async (email, password) => {
-	try {
-		const response = await axios.post(`${AUTH_ENDPOINT}/login`, {
-			email,
-			password,
-		});
-		return response.data;
-	} catch (error) {
-		if (error.response) {
-			throw error.response.status;
-		} else {
-			throw new Error("Network Error");
-		}
-	}
+  try {
+    const response = await axios.post(`${AUTH_ENDPOINT}/login`, {
+      email,
+      password,
+    });
+    console.log('Response:', response);
+
+    if (response && response.data && response.data.tokens && response.data.tokens.refresh) {
+      console.log('Refresh Token:', response.data.tokens.refresh.token);
+      Cookies.set('refreshToken', response.data.tokens.refresh.token, {
+        expires: 7,
+      });
+    } else {
+      throw new Error('Refresh token not found in response');
+    }
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Login failed');
+    }
+  } catch (error) {
+    if (error.response) {
+      throw error.response.status;
+    }
+    throw new Error('An error occurred while logging in.');
+  }
+};
+
+const logout = async (refreshToken) => {
+  try {
+    const response = await axios.post(`${AUTH_ENDPOINT}/logout`, { refreshToken });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      throw error.response.status;
+    } else {
+      throw new Error('Network Error');
+    }
+  }
+};
+
+const refreshAccessToken = async () => {
+  const refreshToken = Cookies.get('refreshToken');
+  if (!refreshToken) throw new Error('No refresh token found');
+
+  try {
+    const response = await axiosConfig.post(`${AUTH_ENDPOINT}/refresh-tokens`, {
+      refreshToken,
+    });
+
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      throw error.response.status;
+    } else {
+      throw new Error('Network Error');
+    }
+  }
 };
 
 const AuthService = {
-	login,
+  login,
+  logout,
+  refreshAccessToken,
 };
 
 export default AuthService;
--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,1 @@
-const API_BASE_URL = "http://localhost:3100/v1";
-
-export const AUTH_ENDPOINT = `${API_BASE_URL}/auth`; // Auth endpoint
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
